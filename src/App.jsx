5e0a4d7a58ccf2c368895f1d2f9100e2ac52e62e
Your Name <92824573+kyleehooley@users.noreply.github.com>
2023-03-03 10:10:00 -0600

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from "react";
-import "./index.css";
-
-import Counter from "./components/Counter";
+import React, { useState, useEffect } from "react"
+import Loading from "./components/Loading"
+import Tours from "./components/Tours"
+const url = "https://course-api.com/react-tours-project"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const fetchTours = async () => {
+    setLoading(true)
+
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
   return (
-    <div>
-      <Counter />
-    </div>
-  );
+    <main>
+      <Tours tours={tours} />
+    </main>
+  )
 }
-export default App;
+
+export default App
