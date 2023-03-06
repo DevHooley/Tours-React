603acf149bfbf7e54523a7391c10779957521b7c
Your Name <92824573+kyleehooley@users.noreply.github.com>
2023-03-06 15:03:56 -0600

    new files

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -3,17 +3,19 @@ import Tour from "./Tour"
 
 const Tours = ({ tours }) => {
   return (
-    <section>
-      <div className="title">
-        <h2>our tours</h2>
-        <div className="underline"></div>
-      </div>
-      <div>
-        {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour}></Tour>
-        })}
-      </div>
-    </section>
+    <div>
+      <section>
+        <div className="title">
+          <h2>our tours</h2>
+          <div className="underline"></div>
+        </div>
+        <div>
+          {tours.map((tour) => {
+            return <Tour key={tour.id} {...tour}></Tour>
+          })}
+        </div>
+      </section>
+    </div>
   )
 }
 
